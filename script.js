c59d4b5b82c72cd1a541bde46548201887910aa9
sangeetha1729 <sangee.mat1729@gmail.com>
2025-05-16 14:36:18 +0100

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function scrollToSection(id) {
     element.scrollIntoView({ behavior: 'smooth' });
   }
 }
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  });
+</script>
